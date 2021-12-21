04b0be36a526df0b2abd178d070ba31423c6ebb1
SputniksDreams <51749588+SputniksDreams@users.noreply.github.com>
2021-12-21 16:00:41 -0600

    figuring out how to grab text values

--- Develop/assets/script.js
+++ Develop/assets/script.js
@@ -1,10 +1,20 @@
-$(".textarea").on("click", "input", function() {
-  var oldtext = $(this)
-  .attr('id')
-  .val();
-  console.log(oldtext);
+stored_entries = {
+  key:"0",
+  input:"test"
+};
+  $(".textarea").on("blur", "input", function() {
+    // console.log("leaving")
   });
 
-  $(".textarea").on("blur", "input", function() {
-    console.log("leaving")
-  });
+$(".textarea").on("click", "input", function() {
+  var text ={ 
+    input: $(this).val(),
+    id: $(this).attr("id")
+    };
+    console.log(text);
+    stored_entries.push(text);
+    console.log(stored_entries);
+    return text;
+});
+  
+window.localStorage.setItem("stored_entries", JSON.stringify(stored_entries));
