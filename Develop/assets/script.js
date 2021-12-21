c4021c99de1a470c6ec9f736db2727329d0892b7
SputniksDreams <51749588+SputniksDreams@users.noreply.github.com>
2021-12-21 16:35:30 -0600

    added blur

--- Develop/assets/script.js
+++ Develop/assets/script.js
@@ -1,20 +1,30 @@
-stored_entries = {
+var stored_entries = {
   key:"0",
   input:"test"
 };
-  $(".textarea").on("blur", "input", function() {
-    // console.log("leaving")
-  });
 
-$(".textarea").on("click", "input", function() {
+var nine_AM = document.querySelector("#zero");
+$(".textarea").on("blur", "input", function() {
   var text ={ 
     input: $(this).val(),
     id: $(this).attr("id")
     };
     console.log(text);
-    stored_entries.push(text);
-    console.log(stored_entries);
-    return text;
-});
-  
+  });
 window.localStorage.setItem("stored_entries", JSON.stringify(stored_entries));
+
+  $(".textarea").on("blur", "input", function() {
+    console.log("leaving")
+  });
+
+// $(".textarea").on("click", "input", function() {
+//   var text ={ 
+//     input: $(this).val(),
+//     id: $(this).attr("id")
+//     };
+//     console.log(text);
+//     stored_entries.push(text);
+//     console.log(stored_entries);
+//     return text;
+// });
+  
